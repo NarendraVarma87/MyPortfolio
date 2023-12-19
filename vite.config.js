b6a4be3026114d19cc2dee6a2a73b240c29b231d
NarendraVarma87 <narendravarma97047@gmail.com>
2023-12-19 15:22:03 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://incredible-babka-bda0a9.netlify.app/',
 })
 
